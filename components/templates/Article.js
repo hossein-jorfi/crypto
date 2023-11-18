93fb7be7434f07d00f66f8264ca49b46d27c9aea
hossein-jorfi <hosseinjorfi81@gmail.com>
2023-11-18 13:43:30 +0330

    articles changed to static elements

--- components/templates/Article.js
+++ components/templates/Article.js
@@ -2,28 +2,127 @@ import Link from "next/link";
 import styles from "../../styles/Article.module.css";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import articleImage from "../../public/article.png"
 
 const Article = () => {
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchBlogData = async () => {
-      const blgData = await fetch(
-        "https://academy.finobit.io/index.php/wp-json/wp/v2/posts?_embed"
-      );
-      const blgJsnData = await blgData.json();
-      const blgJsnData2 = blgJsnData.filter((item) => item.id != "6833");
-      setData(blgJsnData2);
-    };
-    fetchBlogData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchBlogData = async () => {
+  //     const blgData = await fetch(
+  //       "https://academy.finobit.io/index.php/wp-json/wp/v2/posts?_embed"
+  //     );
+  //     const blgJsnData = await blgData.json();
+  //     const blgJsnData2 = blgJsnData.filter((item) => item.id != "6833");
+  //     setData(blgJsnData2);
+  //   };
+  //   fetchBlogData();
+  // }, []);
 
   return (
     <div className="main farsi">
       <h3>مقالات مرتبط</h3>
       <span className={styles.line}></span>
       <div className={`${styles.cardContainer} row pt-3`}>
-        {data ? (
+        
+        <div className="my-2 col-12 col-sm-6 col-md-4">
+          <div className={`${styles.card}`}>
+            <Image
+              unoptimized
+              alt="article image"
+              // loader={() => item._embedded["wp:featuredmedia"]["0"].source_url}
+              src={articleImage}
+              width="150"
+              height="100"
+            />
+            <span>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</span>
+            <Link href="#" className={styles.link} target="_blank">
+              ادامه مطلب
+            </Link>
+          </div>
+        </div>
+        <div className="my-2 col-12 col-sm-6 col-md-4">
+          <div className={`${styles.card}`}>
+            <Image
+              unoptimized
+              alt="article image"
+              // loader={() => item._embedded["wp:featuredmedia"]["0"].source_url}
+              src={articleImage}
+              width="150"
+              height="100"
+            />
+            <span>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</span>
+            <Link href="#" className={styles.link} target="_blank">
+              ادامه مطلب
+            </Link>
+          </div>
+        </div>
+        <div className="my-2 col-12 col-sm-6 col-md-4">
+          <div className={`${styles.card}`}>
+            <Image
+              unoptimized
+              alt="article image"
+              // loader={() => item._embedded["wp:featuredmedia"]["0"].source_url}
+              src={articleImage}
+              width="150"
+              height="100"
+            />
+            <span>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</span>
+            <Link href="#" className={styles.link} target="_blank">
+              ادامه مطلب
+            </Link>
+          </div>
+        </div>
+        <div className="my-2 col-12 col-sm-6 col-md-4">
+          <div className={`${styles.card}`}>
+            <Image
+              unoptimized
+              alt="article image"
+              // loader={() => item._embedded["wp:featuredmedia"]["0"].source_url}
+              src={articleImage}
+              width="150"
+              height="100"
+            />
+            <span>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</span>
+            <Link href="#" className={styles.link} target="_blank">
+              ادامه مطلب
+            </Link>
+          </div>
+        </div>
+        <div className="my-2 col-12 col-sm-6 col-md-4">
+          <div className={`${styles.card}`}>
+            <Image
+              unoptimized
+              alt="article image"
+              // loader={() => item._embedded["wp:featuredmedia"]["0"].source_url}
+              src={articleImage}
+              width="150"
+              height="100"
+            />
+            <span>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</span>
+            <Link href="#" className={styles.link} target="_blank">
+              ادامه مطلب
+            </Link>
+          </div>
+        </div>
+        <div className="my-2 col-12 col-sm-6 col-md-4">
+          <div className={`${styles.card}`}>
+            <Image
+              unoptimized
+              alt="article image"
+              // loader={() => item._embedded["wp:featuredmedia"]["0"].source_url}
+              src={articleImage}
+              width="150"
+              height="100"
+            />
+            <span>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</span>
+            <Link href="#" className={styles.link} target="_blank">
+              ادامه مطلب
+            </Link>
+          </div>
+        </div>
+        
+        {/* {data ? (
           data.map((item) => (
             <div key={item.id} className="my-2 col-12 col-sm-6 col-md-4">
               <div className={`${styles.card}`}>
@@ -46,7 +145,7 @@ const Article = () => {
           ))
         ) : (
           <p className="farsi">در حال گرفتن اطلاعات...</p>
-        )}
+        )} */}
       </div>
     </div>
   );
